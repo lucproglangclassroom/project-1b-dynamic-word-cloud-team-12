52c5a297be184757ff08e1e04c6590a15df02347
AdamShk123 <adamshkolnik5@gmail.com>
2024-09-19 12:49:41 -0500

    added DO comments to see what needs to be done in the concrete implementation

--- src/main/scala/hellotest/ConcreteOutputObserver.scala
+++ src/main/scala/hellotest/ConcreteOutputObserver.scala
@@ -4,11 +4,12 @@ import scala.collection.mutable
 class ConcreteOutputObserver extends OutputObserver:
   override def output(cloud: mutable.Map[String, Int]): Unit = 
   {
-    
+    // DO: call convert function and print result to the standard output for the user
   }
 
   override def convert(cloud: mutable.Map[String, Int]): String = 
   {
+    // DO: convert the given map object into a string representation and return it
     ""
   }
 end ConcreteOutputObserver
