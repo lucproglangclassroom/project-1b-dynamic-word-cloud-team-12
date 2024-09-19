f429dc1a664fda59a6d3656a10ca57c604dc6558
AdamShk123 <adamshkolnik5@gmail.com>
2024-09-18 21:45:08 -0500

    feat: mapped user input into iterator of strings

--- src/main/scala/hellotest/Main.scala
+++ src/main/scala/hellotest/Main.scala
@@ -2,8 +2,18 @@ package hellotest
 
 object Main:
 
-  def main(args: Array[String]) = 
-    println("Hello scalatest!")
-    println(s"Today's date is ${java.time.LocalDate.now}.")
+  def main(args: Array[String]) =
+    val lines = scala.io.Source.stdin.getLines()
+
+    val words = {
+      import scala.language.unsafeNulls
+      lines.flatMap(l => l.split("(?U)[^\\p{Alpha}0-9']+"))
+    }
+
+    for (word <- words)
+    {
+      println("word: " + word)
+      if scala.sys.process.stdout.checkError() then sys.exit(1)
+    }
 
 end Main
