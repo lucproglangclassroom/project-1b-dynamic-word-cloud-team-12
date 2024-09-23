79fa2e74a84fdf51523d5efaed0123c0f26a903b
murphl3 <lmurphy3@luc.edu>
2024-09-23 14:57:40 -0500

    Added Functionality to ConcreteOutputObserver

--- src/main/scala/hellotest/ConcreteOutputObserver.scala
+++ src/main/scala/hellotest/ConcreteOutputObserver.scala
@@ -7,12 +7,20 @@ class ConcreteOutputObserver extends OutputObserver:
   override def output(cloud: Result): Unit =
   {
     // DO: call convert function and print result to the standard output for the user
+    println(convert(cloud))
   }
 
   override def convert(cloud: Result): String =
   {
     // DO: convert the given map object into a string representation and return it
-    ""
+    val keys = cloud.toSeq.sortBy(_._2).reverse.iterator
+    if (!keys.hasNext) return ""
+    var pair = keys.next()
+    var output = pair._1 + ": " + pair._2
+    while (keys.hasNext)
+      pair = keys.next()
+      output += " " + pair._1 + ": " + pair._2
+    return output
   }
 end ConcreteOutputObserver
 
