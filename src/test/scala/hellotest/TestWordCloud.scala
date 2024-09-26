185cef3a5b6f1961a195f254a070211b547bfd79
Liam Rich <liamcr23@gmail.com>
2024-09-26 23:27:20 +0000

    updated test file

--- src/test/scala/hellotest/TestWordCloud.scala
+++ src/test/scala/hellotest/TestWordCloud.scala
@@ -16,27 +16,27 @@ class TestWordCloud extends AnyFunSuite:
 
   test("should have a non-empty count"):
     val wordCloud = new WordCloud(3, 2, 5)
-    val input = List("a", "b", "c", "aa", "bb", "cc", "aa", "bb", "aa", "bb", "a", "b", "c", "a", "a", "a")
+    val input = List("a", "b", "c", "aa", "bb", "cc", "aa", "bb", "aa", "bb", "a", "b", "c", "aa", "aa", "aa")
     val output = new OutputToList()
 
     wordCloud.process(input.iterator, output)
 
-    assert(output.list(0) == mutable.Map[String, Int](("bb", 2), ("aa", 2), ("cc", 1)))
-    assert(output.list(1) == mutable.Map[String, Int](("bb", 2), ("aa", 2), ("cc", 1)))
-    assert(output.list(2) == mutable.Map[String, Int](("bb", 2), ("aa", 2), ("cc", 1)))
-    assert(output.list(3) == mutable.Map[String, Int](("aa", 3), ("bb", 2)))
-    assert(output.list(4) == mutable.Map[String, Int](("aa", 3), ("bb", 2)))
-    assert(output.list(5) == mutable.Map[String, Int](("aa", 4), ("bb", 1)))
+    assert(output.list(0) sameElements Iterator(("bb", 2), ("aa", 2), ("cc", 1)))
+    assert(output.list(1) sameElements Iterator(("bb", 2), ("aa", 2), ("cc", 1)))
+    assert(output.list(2) sameElements Iterator(("bb", 2), ("aa", 2), ("cc", 1)))
+    assert(output.list(3) sameElements Iterator(("aa", 3), ("bb", 2)))
+    assert(output.list(4) sameElements Iterator(("aa", 3), ("bb", 2)))
+    assert(output.list(5) sameElements Iterator(("aa", 4), ("bb", 1)))
 end TestWordCloud
 
 class OutputToList extends OutputObserver:
-  override type Result = mutable.Map[String,Int]
+  override type Result = Iterator[(String, Int)]
 
-  val list: mutable.Queue[mutable.Map[String, Int]] = mutable.Queue[mutable.Map[String, Int]]()
+  val list: mutable.Queue[Result] = mutable.Queue[Result]()
 
   override def output(cloud: Result): Unit =
   {
-    list.append(cloud.clone())
+    list.append(cloud)
   }
 
   override def convert(cloud: Result): String =
@@ -44,3 +44,4 @@ class OutputToList extends OutputObserver:
     ""
   }
 end OutputToList
+
