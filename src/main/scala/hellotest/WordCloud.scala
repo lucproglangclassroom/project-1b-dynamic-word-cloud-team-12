6ece34bfc9bf4a274c910b4008a0112ec557f6b9
Liam Rich <liamcr23@gmail.com>
2024-09-26 21:56:57 +0000

    Trying to fix wordCloud

--- src/main/scala/hellotest/WordCloud.scala
+++ src/main/scala/hellotest/WordCloud.scala
@@ -6,33 +6,37 @@ class WordCloud(c: Int, l: Int, w: Int) {
   private val windowSize = w
 
   def process(input: Iterator[String], output: OutputObserver): Unit = {
-    var window = List[String]()  // Sliding window for the last 'windowSize' words
-    var wordFrequency = scala.collection.mutable.Map[String, Int]()  // To track word frequencies
+    var window = scala.collection.mutable.Queue[String]() // Sliding window for the last 'windowSize' words
+    var wordFrequency = scala.collection.mutable.Map[String, Int]() // To track word frequencies
 
     input.foreach { word =>
       // Step 1: Filter out words that are shorter than 'minLength'
       if (word.length >= minLength) {
         // Step 2: Add the word to the sliding window
-        window = window :+ word
+        window.enqueue(word)
 
         // Step 3: Update the frequency map
         wordFrequency(word) = wordFrequency.getOrElse(word, 0) + 1
 
         // Step 4: If the window exceeds 'windowSize', remove the oldest word
         if (window.size > windowSize) {
-          val oldestWord = window.head
-          window = window.tail
+          val oldestWord = window.dequeue() // Dequeue removes the oldest word
 
           // Decrease frequency of the removed word
           wordFrequency(oldestWord) -= 1
-          if (wordFrequency(oldestWord) == 0) wordFrequency.remove(oldestWord) // Safe discard of Option[Int]
+          if (wordFrequency(oldestWord) == 0) {
+            wordFrequency.remove(oldestWord) // Remove the word if its count reaches zero
+          }
         }
 
         // Step 5: Sort by frequency and pick the top 'cloudSize' words
-        val topWordsList: Iterator[(String, Int)] = wordFrequency.toSeq.sortBy(-_._2).take(cloudSize).toIterator
+        val topWordsMap: scala.collection.mutable.LinkedHashMap[String, Int] =
+          scala.collection.mutable.LinkedHashMap[String, Int](
+            wordFrequency.toSeq.sortBy(-_._2).take(cloudSize): _*
+          )
 
-        // Now you can directly pass topWordsList to output
-        output.output(topWordsList.asInstanceOf[output.Result]) // Ensure type compatibility by casting
+        // Pass the topWordsMap to the output observer
+        output.output(topWordsMap.clone().asInstanceOf[output.Result])
       }
     }
   }
--- src/test/scala/hellotest/TestWordCloud.scala
+++ src/test/scala/hellotest/TestWordCloud.scala
@@ -1,40 +1,46 @@
 package hellotest
 
-class WordCloud(c: Int, l: Int, w: Int) {
-  private val cloudSize = c
-  private val minLength = l
-  private val windowSize = w
+import org.scalatest.funsuite.AnyFunSuite
+import scala.collection.mutable
 
-  def process(input: Iterator[String], output: OutputObserver): Unit = {
-    var window = List[String]()  // Sliding window for the last 'windowSize' words
-    var wordFrequency = scala.collection.mutable.Map[String, Int]()  // To track word frequencies
 
-    input.foreach { word =>
-      // Step 1: Filter out words that are shorter than 'minLength'
-      if (word.length >= minLength) {
-        // Step 2: Add the word to the sliding window
-        window = window :+ word
+class TestWordCloud extends AnyFunSuite:
+  test("should have an empty count"):
+    val wordCloud = new WordCloud(10, 6, 1000)
+    val input = List()
+    val output = new OutputToList()
 
-        // Step 3: Update the frequency map
-        wordFrequency(word) = wordFrequency.getOrElse(word, 0) + 1
+    wordCloud.process(input.iterator, output)
 
-        // Step 4: If the window exceeds 'windowSize', remove the oldest word
-        if (window.size > windowSize) {
-          val oldestWord = window.head
-          window = window.tail
+    assert(output.list.isEmpty)
 
-          // Decrease frequency of the removed word
-          wordFrequency(oldestWord) -= 1
-          if (wordFrequency(oldestWord) == 0) wordFrequency.remove(oldestWord) // Safe discard of Option[Int]
-        }
+  test("should have a non-empty count"):
+    val wordCloud = new WordCloud(3, 2, 5)
+    val input = List("a", "b", "c", "aa", "bb", "cc", "aa", "bb", "aa", "bb", "a", "b", "c", "a", "a", "a")
+    val output = new OutputToList()
 
-        // Step 5: Sort by frequency and pick the top 'cloudSize' words
-        val topWordsMap: scala.collection.mutable.Map[String, Int] =
-          scala.collection.mutable.Map(wordFrequency.toSeq.sortBy(-_._2).take(cloudSize): _*)
+    wordCloud.process(input.iterator, output)
 
-        // Pass the topWordsMap to the output observer
-        output.output(topWordsMap.asInstanceOf[output.Result]) // Ensure type compatibility by casting
-      }
-    }
+    assert(output.list(0) == mutable.Map[String, Int](("bb", 2), ("aa", 2), ("cc", 1)))
+    assert(output.list(1) == mutable.Map[String, Int](("bb", 2), ("aa", 2), ("cc", 1)))
+    assert(output.list(2) == mutable.Map[String, Int](("bb", 2), ("aa", 2), ("cc", 1)))
+    assert(output.list(3) == mutable.Map[String, Int](("aa", 3), ("bb", 2)))
+    assert(output.list(4) == mutable.Map[String, Int](("aa", 3), ("bb", 2)))
+    assert(output.list(5) == mutable.Map[String, Int](("aa", 4), ("bb", 1)))
+end TestWordCloud
+
+class OutputToList extends OutputObserver:
+  override type Result = mutable.Map[String,Int]
+
+  val list: mutable.Queue[mutable.Map[String, Int]] = mutable.Queue[mutable.Map[String, Int]]()
+
+  override def output(cloud: Result): Unit =
+  {
+    list.append(cloud.clone())
   }
-}
+
+  override def convert(cloud: Result): String =
+  {
+    ""
+  }
+end OutputToList
